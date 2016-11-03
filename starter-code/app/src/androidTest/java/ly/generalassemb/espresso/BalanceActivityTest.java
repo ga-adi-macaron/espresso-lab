bac1b283e86bd9c6afb37812c4e01a57f5246980
NikShuvalov <nshuvalov89@gmail.com>
2016-11-03 14:06:02 -0400

    Did bonus to check if the transaction lists are correctly displayed.

--- starter-code/app/src/androidTest/java/ly/generalassemb/espresso/BalanceActivityTest.java
+++ starter-code/app/src/androidTest/java/ly/generalassemb/espresso/BalanceActivityTest.java
@@ -4,11 +4,21 @@ package ly.generalassemb.espresso;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.util.Pair;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -29,6 +39,11 @@ public class BalanceActivityTest {
     @Test
     public void balanceActivityTest() {
 
+        int numOfTransactions= 0;
+        String[][] transactions = {{"Payment from MLM suckers","12482"},{"Gonna try some crack","223.42"},{"Crack is pretty good","421.30"}
+        ,{"Man, I love crack","571"},{"I might be addicted","1378"},{"Just a little more","3202"},{"Senate Paycheck","34750"}};
+
+
         ViewInteraction balanceTextView = onView(withId(R.id.balanceTextView));
         balanceTextView.check(matches(isDisplayed())); //Check if we can see our empty bank account balance.
 
@@ -43,6 +58,7 @@ public class BalanceActivityTest {
         amountEditText.perform(typeText("12482"));
 
         onView(withId(R.id.depositButton)).perform(click()); //At this point we should have gone back to mainscreen.
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$12,482.00")));//Check to see if our balance properly updated to show deposit.
 
@@ -52,6 +68,7 @@ public class BalanceActivityTest {
         amountEditText.perform(typeText("223.42"));
 
         onView(withId(R.id.withdrawButton)).perform(click()); //Takes us back to balance activity
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$12,258.58"))); //Check to see if balance properly updated to show withdrawal.
 
@@ -59,6 +76,7 @@ public class BalanceActivityTest {
         descEditText.perform(typeText("Crack is pretty good"));
         amountEditText.perform(typeText("421.30"));
         onView(withId(R.id.withdrawButton)).perform(click());
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$11,837.28")));
 
@@ -66,6 +84,7 @@ public class BalanceActivityTest {
         descEditText.perform(typeText("Man, I love crack"));
         amountEditText.perform(typeText("571"));
         onView(withId(R.id.withdrawButton)).perform(click());
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$11,266.28")));
 
@@ -73,6 +92,7 @@ public class BalanceActivityTest {
         descEditText.perform(typeText("I might be addicted"));
         amountEditText.perform(typeText("1378"));
         onView(withId(R.id.withdrawButton)).perform(click());
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$9,888.28")));
 
@@ -80,6 +100,7 @@ public class BalanceActivityTest {
         descEditText.perform(typeText("Just a little more"));
         amountEditText.perform(typeText("3202"));
         onView(withId(R.id.withdrawButton)).perform(click());
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$6,686.28")));
 
@@ -87,9 +108,36 @@ public class BalanceActivityTest {
         descEditText.perform(typeText("Senate Paycheck"));
         amountEditText.perform(typeText("34750"));
         onView(withId(R.id.depositButton)).perform(click());
+        numOfTransactions++;
 
         balanceTextView.check(matches(withText("$41,436.28")));
 
+
+        for(int i = 0; i<numOfTransactions;i++){ //I'm not sure if this is doing what I think it's doing, but the idea is that this checks that the transactions match up with what's displayed.
+            childAtPosition(withId(R.id.transactionsListView),i).matches(allOf(withId(R.id.balance_item_description), withText(transactions[i][0])));
+            childAtPosition(withId(R.id.transactionsListView),i).matches(allOf(withId(R.id.balance_item_amount),withText(transactions[i][1])));
+
+        }
+
+
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
